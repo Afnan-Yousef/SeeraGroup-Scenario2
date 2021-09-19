025716191f5b37d4cd4b6f73961ee3db06d0eb0e
Afnan-Yousef <ayousef@aspire-infotech.net>
2021-09-19 20:39:49 +0300

    add any function to commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,16 +10,24 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('any', { prevSubject: 'element' }, (subject, size = 1) => {
+    cy.wrap(subject).then(elementList => {
+      elementList = (elementList.jquery) ? elementList.get() : elementList;
+      elementList = Cypress._.sampleSize(elementList, size);
+      elementList = (elementList.length > 1) ? elementList : elementList[0];
+      cy.wrap(elementList);
+    });
+  });
